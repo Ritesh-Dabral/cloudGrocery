cdafea0777f8004d53b31b0a49bf32b92d58445a
Ritesh <riteshdabral1@gmail.com>
2020-07-15 09:33:46 +0530

    updated fire.js

--- fire.js
+++ fire.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDh2vG1En0Z6tirX_0vzVQfozCCSJ-XSWk",
-    authDomain: "cloud-groceries-886c6.firebaseapp.com",
-    databaseURL: "https://cloud-groceries-886c6.firebaseio.com",
+    apiKey: "YOUR API",
+    authDomain: "YOUR API",
+    databaseURL: "YOUR API",
     projectId: "cloud-groceries-886c6",
-    storageBucket: "cloud-groceries-886c6.appspot.com",
-    messagingSenderId: "602021067798",
-    appId: "1:602021067798:web:19dbe2b0c1da73e74ac937",
-    measurementId: "G-T0CTNNZF64"
+    storageBucket: "YOUR API",
+    messagingSenderId: "YOUR API",
+    appId: "YOUR API",
+    measurementId: "YOUR API"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
